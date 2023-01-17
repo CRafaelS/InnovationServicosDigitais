4c078234eb2fe926215868a5acd039f5d20db2d6
CRafaelS <carlos.rafael@engenharia.ufjf.br>
2023-01-17 14:47:38 -0300

    Iniciando a API, sem uso de DotEnv

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import 'express-async-errors';
+import middlewareError from './middleware/middleware.error';
+import routers from './routes/product.route';
 
 const app = express();
 
 app.use(express.json());
+app.use(routers)
+app.use(middlewareError);
 
 export { app };
